a4f7042b87f5dc0fb2ec9f4b336ace8b02caea96
xiongxiaoyang <773861846@qq.com>
2020-11-22 18:06:01 +0800

    更新sql

--- sql/novel_plus.sql
+++ sql/novel_plus.sql
@@ -1868,3 +1868,5 @@ CREATE TABLE `author_income` (
 
 
 alter table book add column `yesterday_buy` int(11) DEFAULT '0' COMMENT '昨日订阅数' after comment_count;
+
+alter table book_index add column `book_price` int(3) DEFAULT 0 COMMENT '章节费用（屋币）' after `is_vip`;
