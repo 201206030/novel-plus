20469bd66958a87bd520d0df4bbb3b3d27860e43
xiongxiaoyang <773861846@qq.com>
2021-01-19 18:53:47 +0800

    增加生产环境配置

--- novel-admin/src/main/java/com/java2nb/AdminApplication.java
+++ novel-admin/src/main/java/com/java2nb/AdminApplication.java
@@ -2,20 +2,18 @@ package com.java2nb;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//关闭SpringSecurity的功能
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-})
+
 @EnableTransactionManagement
 @ServletComponentScan
 @MapperScan("com.java2nb.*.dao")
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 @EnableCaching
 public class AdminApplication {
     public static void main(String[] args) {
