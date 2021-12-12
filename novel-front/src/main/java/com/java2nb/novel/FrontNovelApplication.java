b2eb6686e9f4264195d6d1826a599a129ebd4419
xiongxiaoyang <773861846@qq.com>
2021-12-12 20:44:09 +0800

    build: 更新xxyopen依赖版本

--- novel-front/src/main/java/com/java2nb/novel/FrontNovelApplication.java
+++ novel-front/src/main/java/com/java2nb/novel/FrontNovelApplication.java
@@ -1,7 +1,6 @@
 package com.java2nb.novel;
 
 import com.github.tobato.fastdfs.FdfsClientConfig;
-import io.shardingsphere.shardingjdbc.spring.boot.SpringBootConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
