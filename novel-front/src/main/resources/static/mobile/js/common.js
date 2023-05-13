dce8a21c0d26966ff5c0032e6b003cf70200c2a3
xiongxiaoyang <1179705413@qq.com>
2023-05-13 13:11:10 +0800

    feat: 首页是否登录识别

--- novel-front/src/main/resources/static/mobile/js/common.js
+++ novel-front/src/main/resources/static/mobile/js/common.js
@@ -1,3 +1,48 @@
+var needLoginPath = ['/user/favorites.html','/user/comment.html','/user/feedback.html',
+    '/user/feedback_list.html','/user/read_history.html','/user/set_name.html',
+    '/user/set_password.html','/user/set_sex.html','/user/setup.html','/user/userinfo.html',
+    "/pay/index.html," +
+    "/author/register.html","/author/index.html"];
+var isLogin = false;
+
+jQuery.cookie = function (name, value, options) {
+    if (typeof value != 'undefined') {
+        options = options || {};
+        if (value === null) {
+            value = '';
+            options.expires = -1;
+        }
+        var expires = '';
+        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+            var date;
+            if (typeof options.expires == 'number') {
+                date = new Date();
+                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+            } else {
+                date = options.expires;
+            }
+            expires = '; expires=' + date.toUTCString();
+        }
+        var path = options.path ? '; path=' + options.path : '';
+        var domain = options.domain ? '; domain=' + options.domain : '';
+        var secure = options.secure ? '; secure' : '';
+        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+    } else {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+};
+
 Array.prototype.indexOf = function (val) {
     for (var i = 0; i < this.length; i++) {
         if (this[i] == val) return i;
@@ -12,14 +57,53 @@ Array.prototype.remove = function (val) {
     }
 };
 
-var token = localStorage.getItem("token");
-if (token) {
-    $.get("/user/isLogin", {"token": token}, function (data) {
-        if (data.code != 1) {//未登录
-            localStorage.removeItem("token");
+var token = $.cookie('Authorization');
+if(!token){
+    if(needLoginPath.indexOf(window.location.pathname) != -1){
+        location.href = '/user/login.html?originUrl='+decodeURIComponent(location.href);
+    }
+
+    $(".user_link").html("<a href=\"/user/login.html\">登录</a>｜<a href=\"/user/register.html\">注册</a>");
+}else{
+    $.ajax({
+        type: "POST",
+        url: "/user/refreshToken",
+        data: {},
+        dataType: "json",
+        success: function(data){
+            if(data.code == 200){
+                $(".user_link").html("<a href=\"/user/userinfo.html\">"+(data.data.nickName.substring(0,3))+"...</a>&nbsp;&nbsp;<a href=\"javascript:logout()\">退出</a>");
+                ;
+                if("/user/login.html" == window.location.pathname){
+                    var orginUrl = getSearchString("originUrl");
+                    window.location.href = orginUrl == undefined || orginUrl.isBlank() ? "/" : orginUrl;
+                    return;
+                }
+                isLogin = true;
+                if(localStorage.getItem("autoLogin") == 1){
+                    $.cookie('Authorization', data.data.token, { expires: 7 ,path: '/'  });
+                }else {
+                    $.cookie('Authorization', data.data.token,{ path: '/'  });
+                }
+            }else{
+                if(needLoginPath.indexOf(window.location.pathname) != -1){
+                    location.href = '/user/login.html';
+                }
+                $(".user_link").html("<a href=\"/user/login.html\">登录</a>｜<a href=\"/user/register.html\">注册</a>");
+            }
+        },
+        error: function () {
+            layer.alert('网络异常');
         }
-    })
+
+    });
 }
+
+function logout() {
+    $.cookie('Authorization', null,{ path: '/'  });
+    location.reload();
+}
+
 
 
 function readHistory() {
